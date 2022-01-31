1b0b9fe8c4df647e1295096acf58331615775e03
Evan Uwao <uwaoevanz@gmail.com>
2022-01-31 15:06:18 +0300

    Managed to save user's information into the database

--- app/src/main/java/com/evan/mymessenger/MainActivity.kt
+++ app/src/main/java/com/evan/mymessenger/MainActivity.kt
@@ -3,13 +3,17 @@ package com.evan.mymessenger
 import android.app.Activity
 import android.content.Intent
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         }
         registerBtn.setOnClickListener {
             performRegister()
+            imageUpload()
     }
         photo_holder.setOnClickListener {
             var intent = Intent(Intent.ACTION_PICK)
@@ -33,36 +38,66 @@ class MainActivity : AppCompatActivity() {
             startActivityForResult(intent,0)
         }
 }
-
+    var selectedPhotoUri: Uri? = null
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode == 0 && resultCode == Activity.RESULT_OK && data != null ){
-            var uri = data?.data
-            var bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+            selectedPhotoUri = data?.data
+            var bitmap = MediaStore.Images.Media.getBitmap(contentResolver, selectedPhotoUri)
             profile_image_signUp.setImageBitmap(bitmap)
             photo_holder.alpha = 0f
         }
     }
 
-private fun performRegister(){
-    val username = username_register.text
-    val email = email_register.text.toString()
-    val password = password_register.text.toString()
-    if(email.isNullOrEmpty() || password.isNullOrEmpty()){
-        Toast.makeText(this, "Invalid Email / Password", Toast.LENGTH_SHORT).show()
-        return
+
+    private fun imageUpload(){
+        if(selectedPhotoUri == null) return
+        val filename = UUID.randomUUID().toString()
+        val ref = FirebaseStorage.getInstance().getReference("images/$filename")
+
+        ref.putFile(selectedPhotoUri!!)
+            .addOnSuccessListener {
+                Log.d("MainActivity", "File stored successfully ${it.metadata?.path}")
+
+                ref.downloadUrl.addOnSuccessListener {
+                    Log.d("MainActivity", "Link $it")
+                    saveUserToDb(it.toString())
+                }
+            }
     }
-//            Log.d("MainActivity", "Email $email")
-    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
-        .addOnCompleteListener {
-            if (it.isSuccessful) {
-                Log.d("MainActivity", "Success ${it.result?.user?.uid}")
-                return@addOnCompleteListener
+
+    private fun saveUserToDb(profileImageUrl: String){
+        val uid = FirebaseAuth.getInstance().uid ?: ""
+        val ref = FirebaseDatabase.getInstance().getReference("/users/${uid}")
+
+        val user = User(uid, username_register.text.toString(),profileImageUrl)
+        ref.setValue(user)
+            .addOnSuccessListener {
+                Log.d("MainActivity", "Finally saved the user to database")
+            }
+    }
+
+    class User(val uid: String, val userName: String, val profileImageUrl: String)
+
+    private fun performRegister(){
+        val username = username_register.text
+        val email = email_register.text.toString()
+        val password = password_register.text.toString()
+        if(email.isNullOrEmpty() || password.isNullOrEmpty()){
+            Toast.makeText(this, "Invalid Email / Password", Toast.LENGTH_SHORT).show()
+            return
+        }
+    //            Log.d("MainActivity", "Email $email")
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d("MainActivity", "Success ${it.result?.user?.uid}")
+                    return@addOnCompleteListener
+                }
+            }
+            .addOnFailureListener{
+                Log.d("MainActivity", "Failed because of ${it.message}")
+                return@addOnFailureListener
             }
         }
-        .addOnFailureListener{
-            Log.d("MainActivity", "Failed because of ${it.message}")
-            return@addOnFailureListener
-        }
     }
-}
